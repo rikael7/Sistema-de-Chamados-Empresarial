f49c18b68a43459ee9eda40d8e4e7f5010ebe6d8
rikael7 <rikaelribeiro52@gmail.com>
2026-09-21 21:56:55 -0300

    fix: bug google auth

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const pgSession = require('connect-pg-simple')(session);
-const { Pool } = require('pg');
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const { Pool } = require("pg");
 const rateLimit = require("express-rate-limit");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 // =======================
 // Login com google
 // ======================
-const session = require("express-session");
+// const session = require("express-session");
 const passport = require("./config/passport");
 
 app.use(passport.initialize());
@@ -20,42 +20,34 @@ app.use(passport.session());
 
 // manda para o google
 app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-        scope: ["profile", "email"]
-    })
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
 );
 
-
-// google retorna (webhook) 
+// google retorna (webhook)
 app.get(
-    "/auth/google/callback",
-    passport.authenticate("google", {
-        failureRedirect: "/login"
-    }),
-    (req, res) => {
-
-        res.redirect("/");
-
-    }
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  },
 );
-
-
-
 
 // -------------
 
-
-
 // RATE LIMIT
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutos
-    limit: 100, // máximo de 100 requisições por IP
-    standardHeaders: "draft-8",
-    legacyHeaders: false,
-    message: {
-        error: "Muitas requisições. Tente novamente mais tarde."
-    }
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  limit: 100, // máximo de 100 requisições por IP
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  message: {
+    error: "Muitas requisições. Tente novamente mais tarde.",
+  },
 });
 // Aplica em TODAS as rotas
 app.use(limiter);
@@ -63,48 +55,50 @@ app.use(limiter);
 // =================
 // Import de Middlewares
 // =============
-const { sanitizeBody, sanitizeQuery } = require('./middleware/sanitize');
-const { isAuthenticated, admin } = require('./middleware/authMiddleware');
+const { sanitizeBody, sanitizeQuery } = require("./middleware/sanitize");
+const { isAuthenticated, admin } = require("./middleware/authMiddleware");
 // middleware para bloquear usuario autenticado de entrar na rota get de register e em login
-const authtrue  = require('./middleware/authtrue');
+const authtrue = require("./middleware/authtrue");
 
 // =================
 // Import de rotas
 // =============
-const authRoutes = require('./routes/authRoutes');
-const publicupload = require('./routes/publicupload');
-const protectedRoutes = require('./routes/protectedRoutes');
-const chamados = require('./routes/chamados');
+const authRoutes = require("./routes/authRoutes");
+const publicupload = require("./routes/publicupload");
+const protectedRoutes = require("./routes/protectedRoutes");
+const chamados = require("./routes/chamados");
 
 // =================
 // websocket
 // =============
-const http = require ('http');
+const http = require("http");
 const server = http.createServer(app);
 
 // =================
 // Pool do Postgree
 // =============
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // =================
 // bloquear Payload gigante
 // =============
-app.use(express.json({ limit: '100kb' }));
-app.use(express.urlencoded({ 
-    extended: true, 
-    limit: '100kb' 
-}));
+app.use(express.json({ limit: "100kb" }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "100kb",
+  }),
+);
 
 // =================
 // enviar front
 // =============
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // =================
 // Sanitização
 // =============
@@ -115,96 +109,82 @@ app.use(sanitizeQuery);
 // Seções do Postgree
 // =============
 app.use(
-    session({
-        store: new pgSession({
-            pool: pool,
-            tableName: 'sessions',
-            createTableIfMissing: true
-        }),
+  session({
+    store: new pgSession({
+      pool: pool,
+      tableName: "sessions",
+      createTableIfMissing: true,
+    }),
 
-        key: 'connect.sid',
+    key: "connect.sid",
 
-        secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
 
-        resave: false,
+    resave: false,
 
-        saveUninitialized: false,
+    saveUninitialized: false,
 
-        cookie: {
-            httpOnly: true,
+    cookie: {
+      httpOnly: true,
 
-            secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === "production",
 
-            maxAge: 1000 * 60 * 60 * 24 // 1 dia
-        }
-    })
+      maxAge: 1000 * 60 * 60 * 24, // 1 dia
+    },
+  }),
 );
 // =================
 // enviar front
 // =============
 
-app.get('/', isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+app.get("/", isAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "dashboard.html"));
 });
 
-app.get('/admin', isAuthenticated, admin, (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'admin.html'));
+app.get("/admin", isAuthenticated, admin, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "admin.html"));
 });
-
-
 
 // =================
 // frontend publico
 // =============
 
-app.get('/login', authtrue, (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+app.get("/login", authtrue, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
-
-app.get('/register', authtrue, (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+app.get("/register", authtrue, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "register.html"));
 });
 
-
-app.get('/404', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+app.get("/404", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "404.html"));
 });
-
 
 // ROTAS API
-app.use('/auth', authRoutes);
-app.use('/api', chamados);
-
-
+app.use("/auth", authRoutes);
+app.use("/api", chamados);
 
 // Erro genérico
 app.use((err, req, res, next) => {
-    console.error('Erro não tratado:', err);
-    res.status(500).json({
-        error: 'Erro interno do servidor.'
-    });
+  console.error("Erro não tratado:", err);
+  res.status(500).json({
+    error: "Erro interno do servidor.",
+  });
 });
-
 
 // se não encontrar nenhuma rota
 // Middleware 404 (sempre por último pois o node le de cima para baixo as rotas, caso não encontre nada vai cair nessa)
 app.use((req, res) => {
-    res.redirect("/404");
+  res.redirect("/404");
 });
 
-
-
-
-
 server.listen(PORT, () => {
- console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 // ================
 
-
-
-// 
+//
 // app.listen(PORT, () => {
 //     console.log(`Servidor rodando na porta ${PORT}`);
 // });
