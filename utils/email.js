451ed876079e0cb37ddb1c3183bfc556b604fcb1
rikael7 <rikaelribeiro52@gmail.com>
2026-09-24 05:33:37 -0300

    fix: configuração SMIP usando porta 587

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
 
     auth: {
         user: process.env.EMAIL_USER,
